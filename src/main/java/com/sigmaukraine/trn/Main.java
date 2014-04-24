542a682f181ac1800fcccaa15793494dab0fd637
Mark Two <mkulava@icloud.com>
2014-04-24 14:08:49 +0300

    Initial commit

--- src/main/java/com/sigmaukraine/trn/Main.java
+++ src/main/java/com/sigmaukraine/trn/Main.java
@@ -2,14 +2,45 @@ package com.sigmaukraine.trn;
 
 import com.sigmaukraine.trn.testEntities.TestSuite;
 
-import java.lang.String;
+import java.util.LinkedList;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args){
-        TestSuite preRequisites = new TestSuite("Pre-requisites KDT Scenario 1");
-        preRequisites.execute();
+        List<TestSuite> testScenarios = new LinkedList<TestSuite>();
 
-        TestSuite KDTscenario1 = new TestSuite("KDT Scenario 1");
-        KDTscenario1.execute();
+        testScenarios.add(new TestSuite("Pre-requisites KDT Scenario 1"));
+        testScenarios.add(new TestSuite("KDT Scenario 1"));
+        testScenarios.add(new TestSuite("KDT Scenario 2"));
+
+        for (TestSuite testSuite : testScenarios){
+            testSuite.execute();
+        }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
